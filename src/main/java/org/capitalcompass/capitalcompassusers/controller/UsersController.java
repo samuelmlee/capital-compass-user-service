bf51ae034bc8d97287c5b1b00981de0b5c0f392c
Samuel Lee <sam.leefr@gmail.com>
2023-11-27 13:17:22 +0100

    v0.1

--- src/main/java/org/capitalcompass/capitalcompassusers/controller/UsersController.java
+++ src/main/java/org/capitalcompass/capitalcompassusers/controller/UsersController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/v1/users")
 public class UsersController {
 
     @GetMapping("/status")
