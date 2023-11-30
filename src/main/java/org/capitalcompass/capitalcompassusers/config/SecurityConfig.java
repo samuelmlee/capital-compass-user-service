11e22c2917a83b994719c1dad74357fbd87d74dc
Samuel Lee <sam.leefr@gmail.com>
2023-11-30 15:42:18 +0100

    claim from token forwarded

---
+++ src/main/java/org/capitalcompass/capitalcompassusers/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package org.capitalcompass.capitalcompassusers.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.
+                authorizeHttpRequests(authorize -> authorize
+                        .mvcMatchers(HttpMethod.GET, "/users/watchlists/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthorityPrefix("ROLE_");
+        authoritiesConverter.setAuthoritiesClaimName("role");
+
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return authenticationConverter;
+    }
+}
--- src/main/java/org/capitalcompass/capitalcompassusers/controller/WatchListsController.java
+++ src/main/java/org/capitalcompass/capitalcompassusers/controller/WatchListsController.java
@@ -3,23 +3,29 @@ package org.capitalcompass.capitalcompassusers.controller;
 import lombok.RequiredArgsConstructor;
 import org.capitalcompass.capitalcompassusers.model.Watchlist;
 import org.capitalcompass.capitalcompassusers.service.WatchListService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users/watchlists")
+@RequestMapping("/v1/users/watchlists")
 @RequiredArgsConstructor
 public class WatchListsController {
 
     private final WatchListService watchListService;
 
     @GetMapping
-    public ResponseEntity<List<Watchlist>> getWatchListsForUser() {
-        List<Watchlist> lists = this.watchListService.getWatchListsForUser("test");
+    public ResponseEntity<List<Watchlist>> getWatchListsForUser(Principal principal) {
+        if (principal == null) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        String userSub = principal.getName();
+        List<Watchlist> lists = this.watchListService.getWatchListsForUser(userSub);
         return ResponseEntity.ok(lists);
     }
 
