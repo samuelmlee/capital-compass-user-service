cc7bde3d15ea93347b5d9b320c0b7485a4cdfb6b
Samuel Lee <sam.leefr@gmail.com>
2024-01-03 11:35:57 +0100

    renaming

--- src/main/java/org/capitalcompass/capitalcompassusers/UserServiceApplication.java
+++ src/main/java/org/capitalcompass/capitalcompassusers/UserServiceApplication.java
@@ -6,12 +6,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class CapitalCompassUsersApplication {
+public class UserServiceApplication {
 
 
     public static void main(String[] args) {
 
-        SpringApplication.run(CapitalCompassUsersApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 
 
--- src/test/java/org/capitalcompass/capitalcompassusers/UserServiceApplicationTests.java
+++ src/test/java/org/capitalcompass/capitalcompassusers/UserServiceApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Disabled
-class CapitalCompassUsersApplicationTests {
+class UserServiceApplicationTests {
 
     @Test
     void contextLoads() {
