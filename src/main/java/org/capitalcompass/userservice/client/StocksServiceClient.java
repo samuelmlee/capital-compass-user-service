9d04d7d6db5c3ba1070d221054d5d5037ea32a68
Samuel Lee <sam.leefr@gmail.com>
2024-01-17 00:43:18 +0100

    clean up client

--- src/main/java/org/capitalcompass/userservice/client/StocksServiceClient.java
+++ src/main/java/org/capitalcompass/userservice/client/StocksServiceClient.java
@@ -5,12 +5,9 @@ import lombok.extern.log4j.Log4j2;
 import org.capitalcompass.userservice.dto.ValidateTickerRequestDTO;
 import org.capitalcompass.userservice.exception.TickerSymbolsNotValidatedException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Set;
@@ -38,29 +35,22 @@ public class StocksServiceClient {
      *                                            the ticker symbols or in communication with the stocks service.
      */
     public Set<String> registerTickers(Set<String> tickerSymbols) {
-        String STOCKS_SERVICE_URL = stockServiceUri + "/v1/stocks/reference/tickers";
 
         ValidateTickerRequestDTO request = ValidateTickerRequestDTO.builder()
                 .symbols(tickerSymbols)
                 .build();
         HttpEntity<ValidateTickerRequestDTO> entity = new HttpEntity<>(request);
 
-        try {
-            ResponseEntity<Set<String>> response = restTemplate.exchange(
-                    STOCKS_SERVICE_URL + "/register", HttpMethod.POST, entity,
-                    new ParameterizedTypeReference<Set<String>>() {
-                    });
+        ResponseEntity<String[]> response = restTemplate.postForEntity(
+                stockServiceUri + "/v1/stocks/reference/tickers/register", entity, String[].class);
 
-            if (!response.getStatusCode().is2xxSuccessful()) {
-                throw new TickerSymbolsNotValidatedException("Error registering and validating ticker symbols. HTTP status: "
-                        + response.getStatusCode());
-            }
-            return response.getBody();
-
-        } catch (RestClientException e) {
-            throw new TickerSymbolsNotValidatedException("Error communicating with the stocks service: "
-                    + e.getMessage());
+        if (!response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
+            throw new TickerSymbolsNotValidatedException("Error registering and validating ticker symbols. HTTP status: "
+                    + response.getStatusCode());
         }
+        return Set.of(response.getBody());
+
+
     }
 
 
